66fc9f324ae7ff329f45170522ac62b02e5f1ff8
Matan Broner <matanbroner@gmail.com>
2023-01-23 13:35:16 -0600

    test raw sock

--- src/server/proxy.py
+++ src/server/proxy.py
@@ -1,89 +1,23 @@
 """
 TCP Proxy Server
+Uses raw sockets to listen for incoming TCP packets and 
+forwards them to the nested host and port based on the Convert Protocol TLV
 """
-import socket
-import select
-import time
-import sys
-
-# Changing the buffer_size and delay, you can improve the speed and bandwidth.
-# But when buffer get to high or delay go too down, you can broke things
-buffer_size = 4096
-delay = 0.0001
-forward_to = ('smtp.zaz.ufsk.br', 25)
-
-class Forward:
-    def __init__(self):
-        self.forward = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-
-    def start(self, host, port):
-        try:
-            self.forward.connect((host, port))
-            return self.forward
-        except Exception as e:
-            print(e)
-            return False
 
 class TCPProxy:
-    input_list = []
-    channel = {}
-
-    def __init__(self, host, port):
-        self.server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-        self.server.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
-        self.server.bind((host, port))
-        self.server.listen(200)
-        print("TCP Proxy Server started on %s:%d" % (host, port))
-
-    def run(self):
-        self.input_list.append(self.server)
-        while 1:
-            time.sleep(delay)
-            ss = select.select
-            inputready, outputready, exceptready = ss(self.input_list, [], [])
-            for self.s in inputready:
-                print(self.s)
-                if self.s == self.server:
-                    self.on_accept()
-                    break
-
-                self.data = self.s.recv(buffer_size)
-                if len(self.data) == 0:
-                    self.on_close()
-                    break
-                else:
-                    self.on_recv()
-
-    def on_accept(self):
-        forward = Forward().start(forward_to[0], forward_to[1])
-        clientsock, clientaddr = self.server.accept()
-        if forward:
-            print(clientaddr, "has connected")
-            self.input_list.append(clientsock)
-            self.input_list.append(forward)
-            self.channel[clientsock] = forward
-            self.channel[forward] = clientsock
-        else:
-            print("Can't establish connection with remote server.")
-            print("Closing connection with client side", clientaddr)
-            clientsock.close()
-
-    def on_close(self):
-        print(self.s.getpeername(), "has disconnected")
-        #remove objects from input_list
-        self.input_list.remove(self.s)
-        self.input_list.remove(self.channel[self.s])
-        out = self.channel[self.s]
-        # close the connection with client
-        self.channel[out].close()  # equivalent to do self.s.close()
-        # close the connection with remote server
-        self.channel[self.s].close()
-        # delete both objects from channel dict
-        del self.channel[out]
-        del self.channel[self.s]
-
-    def on_recv(self):
-        data = self.data
-        # here we can parse and/or modify the data before send forward
+    def __init__(self, host: str, port: int):
+        self.host = host
+        self.port = port
+        self.connections = {}
+        
+        # Create raw socket to listen for incoming TCP packets
+        self.socket = socket.socket(socket.AF_INET, socket.SOCK_RAW, socket.IPPROTO_TCP)
+        self.socket.bind((self.host, self.port))
+        
+        # Listen for incoming packets
+        self.listen()
+        
+    def listen(self):
+        data = self.socket.recvfrom(65535)
         print(data)
-        self.channel[self.s].send(data)
+        
