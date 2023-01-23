122d882c824df9130f8944c9fbb83920071f05de
Matan Broner <matanbroner@gmail.com>
2023-01-23 14:05:27 -0600

    try parse packet tlv

--- src/server/proxy.py
+++ src/server/proxy.py
@@ -7,6 +7,8 @@ forwards them to the nested host and port based on the Convert Protocol TLV
 import socket
 from scapy.all import *
 
+from src.protocol import *
+
 class TCPProxy:
     def __init__(self, port, on_packet):
         self.port = port
@@ -18,6 +20,14 @@ class TCPProxy:
     def on_packet(self, packet: scapy.packet.Packet):
         if packet[TCP].dport == self.port:
             print(f"IP: {packet[IP].src}:{packet[TCP].sport} -> {packet[IP].dst}:{packet[TCP].dport}")
+            # if initial packet (SYN)
+            if packet[TCP].flags == "S":
+                # get the nested host and port from the Convert Protocol TLV
+                # located in the SYN payload
+                # parse the payload as a Convert Protocol TLV
+                payload = packet[TCP].payload
+                tlv = ConvertProtocolFixedHeader(payload)
+                tlv.parse()
             self.client_on_packet(packet)
         
         
