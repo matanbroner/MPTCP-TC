718a2c47c86ae8d3d008c125dc05a9b31a58aa84
Matan Broner <matanbroner@gmail.com>
2023-01-23 11:51:33 -0600

    config update schema

--- src/server/config.py
+++ src/server/config.py
@@ -12,7 +12,6 @@ class Config:
         with open(filename, "r") as f:
             config = yaml.safe_load(f)
             config = config["server"]
-            self.interfaces = config["interfaces"]
             self.host = config["host"]
             self.port = config["port"]
     
