74f8a5783a37242c5e69db81e3e6890bb3cbad31
Matan Broner <matanbroner@gmail.com>
2023-01-23 13:42:40 -0600

    sniff with scapy

--- src/server/proxy.py
+++ src/server/proxy.py
@@ -1,10 +1,11 @@
 """
 TCP Proxy Server
-Uses raw sockets to listen for incoming TCP packets and 
+Uses Scapy to listen for incoming TCP packets and
 forwards them to the nested host and port based on the Convert Protocol TLV
 """
 
 import socket
+from scapy.all import *
 
 class TCPProxy:
     def __init__(self, host: str, port: int):
@@ -12,14 +13,9 @@ class TCPProxy:
         self.port = port
         self.connections = {}
         
-        # Create raw socket to listen for incoming TCP packets
-        self.socket = socket.socket(socket.AF_INET, socket.SOCK_RAW, socket.IPPROTO_TCP)
-        self.socket.bind((self.host, self.port))
+        sniff(filter="tcp", prn=self.on_packet)
         
-        # Listen for incoming packets
-        self.listen()
-        
-    def listen(self):
-        data = self.socket.recvfrom(65535)
-        print(data)
+    
+    def on_packet(self, pkt):
+        pkt.show()
         
