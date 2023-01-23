d469cd51bf63d9437027700fdde73188897e1c86
Matan Broner <matanbroner@gmail.com>
2023-01-23 13:16:06 -0600

    ots tcp proxy test

--- src/server/proxy.py
+++ src/server/proxy.py
@@ -1,51 +1,87 @@
 """
 TCP Proxy Server
 """
-
 import socket
 import select
+import time
 import sys
 
-class TCPProxy:
-    def __init__(self, host, port) -> None:
-        self.host = host
-        self.port = port
-        self.lsock = []
-        
-        
-    def run(self) -> None:
-        s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+# Changing the buffer_size and delay, you can improve the speed and bandwidth.
+# But when buffer get to high or delay go too down, you can broke things
+buffer_size = 4096
+delay = 0.0001
+forward_to = ('smtp.zaz.ufsk.br', 25)
+
+class Forward:
+    def __init__(self):
+        self.forward = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
+    def start(self, host, port):
         try:
-            s.bind((self.host, self.port))
-            s.listen(5)
-            self.lsock.append(s)
-            print('TCP Proxy listening on port', self.port)
-            while True:
-                rsock, wsock, esock = select.select(self.lsock, [], [])
-                for sock in rsock:
-                    print('Got a socket')
-                    if sock == s:
-                        self.accept_new_connection(s)
-                    else:
-                        self.data_from_client(sock)
-        except KeyboardInterrupt:
-            print('Exiting')
-            s.close()
-            sys.exit(0)
-    
-    def accept_new_connection(self, s):
-        (csock, addr) = s.accept()
-        self.lsock.append(csock)
-        print('Got connection from', addr)
-    
-    def data_from_client(self, sock):
-        data = sock.recv(1024)
-        if not data:
-            print('Closing connection to client')
-            self.lsock.remove(sock)
-            sock.close()
-            return
-        print('Client sent: ', data)
-        sock.send(data)
-        
-        
+            self.forward.connect((host, port))
+            return self.forward
+        except Exception as e:
+            print(e)
+            return False
+
+class TCPProxy:
+    input_list = []
+    channel = {}
+
+    def __init__(self, host, port):
+        self.server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        self.server.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+        self.server.bind((host, port))
+        self.server.listen(200)
+
+    def run(self):
+        self.input_list.append(self.server)
+        while 1:
+            time.sleep(delay)
+            ss = select.select
+            inputready, outputready, exceptready = ss(self.input_list, [], [])
+            for self.s in inputready:
+                if self.s == self.server:
+                    self.on_accept()
+                    break
+
+                self.data = self.s.recv(buffer_size)
+                if len(self.data) == 0:
+                    self.on_close()
+                    break
+                else:
+                    self.on_recv()
+
+    def on_accept(self):
+        forward = Forward().start(forward_to[0], forward_to[1])
+        clientsock, clientaddr = self.server.accept()
+        if forward:
+            print(clientaddr, "has connected")
+            self.input_list.append(clientsock)
+            self.input_list.append(forward)
+            self.channel[clientsock] = forward
+            self.channel[forward] = clientsock
+        else:
+            print("Can't establish connection with remote server.")
+            print("Closing connection with client side", clientaddr)
+            clientsock.close()
+
+    def on_close(self):
+        print(self.s.getpeername(), "has disconnected")
+        #remove objects from input_list
+        self.input_list.remove(self.s)
+        self.input_list.remove(self.channel[self.s])
+        out = self.channel[self.s]
+        # close the connection with client
+        self.channel[out].close()  # equivalent to do self.s.close()
+        # close the connection with remote server
+        self.channel[self.s].close()
+        # delete both objects from channel dict
+        del self.channel[out]
+        del self.channel[self.s]
+
+    def on_recv(self):
+        data = self.data
+        # here we can parse and/or modify the data before send forward
+        print(data)
+        self.channel[self.s].send(data)
